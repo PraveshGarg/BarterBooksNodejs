2553a17452d0b68a1997e8c1a6a2b6d11a030f65
Garg <PGarg@in.imshealth.com>
2021-02-24 21:43:54 +0530

    change port to default for heroku

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ var nodemailer = require('nodemailer');
 //-----------------------------------------------------------------------------------
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
